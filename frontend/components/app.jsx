606f45d2bd3b02057f8c0499c01321d50a367b4c
Christina <chu.christina@yahoo.com>
2018-12-10 16:18:00 -0800

    Create auth routes

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,13 +3,14 @@ import { Route } from "react-router-dom"
 import GreetingContainer from "./greeting/greeting_container";
 import SignupFormContainer from "./session_form/signup_form_container";
 import LoginFormContainer from "./session_form/login_form_container";
+import { AuthRoute } from "../util/route_util";
 
 const App = () => (
   <div>
     <h1>Bench BnB</h1>
     <GreetingContainer />
-    <Route path="/login" component={LoginFormContainer} />
-    <Route path="/signup" component={SignupFormContainer} />
+    <AuthRoute path="/login" component={LoginFormContainer} />
+    <AuthRoute path="/signup" component={SignupFormContainer} />
   </div>
 
 );
--- frontend/util/route_util.js
+++ frontend/util/route_util.js
@@ -1,4 +1,6 @@
-import { connect } from 'react-redux'
+import React from 'react';
+import { connect } from 'react-redux';
+import { Route, Redirect, withRouter } from "react-router-dom";
 
 const Auth = ({ component: Component, path, loggedIn, exact }) => (
   <Route path={path} exact={exact} render={(props) => (

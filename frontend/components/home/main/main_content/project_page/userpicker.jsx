07b6a2b0d43467ced7861af1a78cc5843139acd9
Christina <chu.christina@yahoo.com>
2018-12-19 17:58:45 -0800

    Add userpicker

--- frontend/components/home/main/main_content/project_page/userpicker.jsx
+++ frontend/components/home/main/main_content/project_page/userpicker.jsx
@@ -41,25 +41,53 @@ export default class UserPicker extends React.Component {
     this.setState({ inputVal: name });
   }
 
-  render() {
+  renderNameList() {
     const results = this.matches().map((match, i) => {
       return (
-        <li key={i} onClick={this.selectName}>{match}</li>
+        <li 
+          key={i} 
+          onClick={this.selectName}>{match}</li>
       );
     });
 
     return (
-      <div>
-        <div className='auto'>
+      <div className="dropdown">
+        <ul id="assignee-dropdown" className="dropdown-content-assignee">
+          {results}
+        </ul>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div className='assignee-section'>
+        <div className='assignee-button' onClick={handleDropdownClick("assignee-dropdown")}>
+          <div className="dashed-circle-icon">
+              <svg viewBox="0 0 32 32">
+                <path d="M16,18c-4.4,0-8-3.6-8-8s3.6-8,8-8s8,3.6,8,8S20.4,18,16,18z M16,4c-3.3,0-6,2.7-6,6s2.7,6,6,6s6-2.7,6-6S19.3,4,16,4z" />
+                <path d="M29,32c-0.6,0-1-0.4-1-1v-4.2c0-2.6-2.2-4.8-4.8-4.8H8.8C6.2,22,4,24.2,4,26.8V31c0,0.6-0.4,1-1,1s-1-0.4-1-1v-4.2C2,23,5,20,8.8,20h14.4c3.7,0,6.8,3,6.8,6.8V31C30,31.6,29.6,32,29,32z" />
+              </svg>
+            </div>
           <input
             onChange={this.handleInput}
             value={this.state.inputVal}
-            placeholder='Search...' />
-          <ul>
-            {results}
-          </ul>
+            placeholder='Name' />
+          {this.renderNameList()}
         </div>
       </div>
     );
   }
 };
+
+// <div className="assignee-section">
+//   <div className="assignee-button">
+//     <div className="dashed-circle-icon">
+//       <svg viewBox="0 0 32 32">
+//         <path d="M16,18c-4.4,0-8-3.6-8-8s3.6-8,8-8s8,3.6,8,8S20.4,18,16,18z M16,4c-3.3,0-6,2.7-6,6s2.7,6,6,6s6-2.7,6-6S19.3,4,16,4z" />
+//         <path d="M29,32c-0.6,0-1-0.4-1-1v-4.2c0-2.6-2.2-4.8-4.8-4.8H8.8C6.2,22,4,24.2,4,26.8V31c0,0.6-0.4,1-1,1s-1-0.4-1-1v-4.2C2,23,5,20,8.8,20h14.4c3.7,0,6.8,3,6.8,6.8V31C30,31.6,29.6,32,29,32z" />
+//       </svg>
+//     </div>
+//     <div>Unassigned</div>
+//   </div>
+// </div>
